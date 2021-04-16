79666c0fb8acd5b38e878e77340f326680eb9ad1
Kevin Spoth <kspoth08@su.edu>
2021-04-16 16:45:23 -0400

    adding homepage and navbar

---
+++ src/components/layout/Main.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export class Main extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="photo-container">
+          <img
+            src="assets/ProfilePic.jpg"
+            alt="Kevin Spoth's Profile Picture"
+            className="card-img bio-image"
+          />
+        </div>
+        <h5 className="title-name">Kevin Spoth</h5>
+        <p className="short-bio">About me section</p>
+        <div className="row justify-content-center short-bio links-row">
+          <div className="col-auto">
+            {/* change to redirect to new component */}
+            <Link className="highlight-me" to="/projects">
+              Portfolio
+            </Link>
+          </div>
+          <div className="col-auto">
+            <a
+              className="highlight-me"
+              href="assets/Resume Kevin Spoth 3-25-2021 FSW.pdf"
+              download="Kevin_Spoth_Resume"
+            >
+              Resume
+            </a>
+          </div>
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <p className="descrp-text">
+              <i className="fas fa-envelope"></i> ks2145@email.vccs.edu
+            </p>
+          </div>
+          <div className="col-auto">
+            <p className="descrp-text">
+              <i className="fas fa-phone"></i> (703) 498-9381
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <span className="navbar-brand mb-0 h1"> </span>
+      <ul className="nav justify-content-end">
+        <Link className="fas fa-home highlight-me icon-link" to="/"></Link>
+        <a
+          className="fab fa-github highlight-me icon-link"
+          href="https://github.com/kspoth"
+        ></a>
+        <a
+          className="fab fa-linkedin highlight-me icon-link"
+          href="https://www.linkedin.com/in/kevin-spoth-4924034b/"
+        ></a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
