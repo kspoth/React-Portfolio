d66a29dd69249bd6ba58aa92bc8b29bf7e57730f
Kevin Spoth <kspoth08@su.edu>
2021-04-17 22:50:30 -0400

    fixing typos

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -14,16 +14,15 @@ export class Main extends Component {
         </div>
         <h5 className="title-name">Kevin Spoth</h5>
         <p className="short-bio">
-          Asprirng full stack web developer at the University of Richmond's
+          Aspiring full stack web developer at the University of Richmond's
           Full-Stack Web Development Bootcamp. I am familiarizing myself with
           programming technologies such as JavaScript, HTML, CSS, Web API's,
-          NodeJS, JQuery, MySQL, MongoDB, DOM Manipulation, ReactJS, Git and
-          Github through working on projects and daily programming excercises. I
-          am certain that my passion and interest in technology and business
-          background will benefit your company. My determination and desire to
-          achieve will benefit your team. I look forward to my future as a
-          software developer and would certainly like the chance to discuss how
-          my qualifications meet your company’s needs
+          Heroku NodeJS, JQuery, MySQL, MongoDB, DOM Manipulation, ReactJS, Git
+          and Github through working on projects and daily programming
+          exercises. I am certain that my passion and interest in technology and
+          business background will benefit your orginization. I look forward to
+          my future as a software developer and would certainly like the chance
+          to discuss how my qualifications meet your company’s needs.
         </p>
         <div className="row justify-content-center short-bio links-row">
           <div className="col-auto">
